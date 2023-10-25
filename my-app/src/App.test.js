958157b5068bad614331593de8c5a6d0d1b3e6c8
YusupOzdoyev <yusup.ozdoyev@mail.ru>
2023-10-25 21:45:26 +0300

    cleaning project

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -3,6 +3,11 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloWorldElem = screen.getByText(/Hello World!/i);
+  const btn = screen.getByRole('button');
+  const input = screen.getByPlaceholderText(/input value/i);
+  expect(helloWorldElem).toBeInTheDocument();
+  expect(btn).toBeInTheDocument();
+  expect(input).toBeInTheDocument();
+  // screen.debug()
 });
